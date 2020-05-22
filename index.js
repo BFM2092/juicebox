6f1d7d613b0f068529c7f2fa9fecb2b673ec9161
BFM2092 <56615597+BFM2092@users.noreply.github.com>
2020-05-21 20:12:35 -0400

    day 2 done

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
+
 const PORT = 3000;
 
 const express = require('express');
 
 const server = express();
 
+const bodyParser = require('body-parser');
+
+
 const { client } = require('./db');
 client.connect();
+
+server.use(bodyParser.json());
 
 server.listen(PORT, () => {
     console.log('The server is up on port', PORT)
@@ -22,9 +29,6 @@ server.listen(PORT, () => {
 
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
-
-const bodyParser = require('body-parser');
-server.use(bodyParser.json());
 
 const morgan = require('morgan');
 server.use(morgan('dev'));
